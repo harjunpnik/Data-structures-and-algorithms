19dc0ad2be87fb1bd0c449379d40179eb79b2fe4
harjunpnik <niklasharjunpaa@gmail.com>
2019-02-15 23:51:42 +0200

    Initial commit to course project 1

---
+++ Pathfinding/src/Node.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Node {
+
+	private String name;
+	private double latitude;
+	private double longitude;
+	private ArrayList<Node> neighbours;
+	
+	//	Constructor
+	public Node(String name, double longitude, double latitude) {
+		setName(name);
+		setLatitude(latitude);
+		setLongitude(longitude);
+	}
+	
+	//	Name
+	public String getName() {
+		return name;
+	}
+	
+	private void setName(String name){
+		this.name = name;
+	}
+	
+	//	Latitude
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	private void setLatitude(double latitude){
+		this.latitude = latitude;
+	}
+	
+	//	Longitude
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	private void setLongitude(double longitude){
+		this.longitude = longitude;
+	}
+	
+	//	Neighbours
+	public void addNeighbour(Node neighbour) {
+		neighbours.add(neighbour);
+	}
+	
+	
+	public ArrayList<Node> getNeighbours() {
+		return neighbours;
+	}
+	
+	
+	
+}
---
+++ Pathfinding/src/Pathfinding.java
@@ -0,0 +1,13 @@
+
+public class Pathfinding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Functions functions = new Functions();
+		 
+		 functions.createGraph();
+
+	}
+
+}
