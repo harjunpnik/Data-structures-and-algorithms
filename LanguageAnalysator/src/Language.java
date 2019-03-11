bfa5aecd21dfb1a473310a100635f3ccebe3ae41
harjunpnik <niklasharjunpaa@gmail.com>
2019-03-11 17:55:45 +0200

    Testing First Char Distribution

--- LanguageAnalysator/src/Language.java
+++ LanguageAnalysator/src/Language.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Language {
@@ -6,7 +7,7 @@ public class Language {
 	private ArrayList<String> content;
 	private String languageLabel;
 	private HashMap <String, Double> singelCharDistribution;
-	private HashMap <String, Double> firstCharDistribution;
+	private HashMap <String, Double> firstCharDistribution = new HashMap<String, Double>();
 	private HashMap <String, Double> threeCharDistribution;
 	
 	public Language(String languageLabel, ArrayList<String> content) {
@@ -29,4 +30,58 @@ public class Language {
 	public ArrayList <String> getContent(){
 		return content;
 	}
+	
+	public String getString() {
+		String listString = String.join("", content).toLowerCase();
+		
+		return listString;
+	}
+	
+	public String getStringFirstChar() {
+		String tempText = ""; 
+		for(String str : content) {
+			tempText += str.toLowerCase().charAt(0);
+		}
+		System.out.println(tempText);
+		char[] chars = tempText.toCharArray();
+		Arrays.sort(chars);
+		String sortedText = new String(chars);
+		
+		System.out.println(sortedText.length());
+		System.out.println(sortedText);
+		
+		double charDistributionLength = sortedText.length();
+		
+		for(char character : chars) {
+			//System.out.print(character);
+			String tempChar = String.valueOf(character);
+			
+			//if hashmap contains key then 
+			// hashmap.put word , word.get +1
+			//else
+			// hahsmap .put word , 1
+			if(firstCharDistribution.containsKey(tempChar)) {
+				//System.out.println("true");
+				firstCharDistribution.put(tempChar,firstCharDistribution.get(tempChar) + 1);
+			}else {
+				firstCharDistribution.put(tempChar, 1.);
+			}
+		}
+		
+		//Loop through list and assign percentage value
+		for(String key : firstCharDistribution.keySet()) {
+			System.out.print(key);
+			System.out.print(firstCharDistribution.get(key));
+			firstCharDistribution.put(key, firstCharDistribution.get(key) / charDistributionLength );
+		}
+		
+		System.out.println();
+		System.out.println(firstCharDistribution);
+
+		
+		
+		
+		return sortedText;
+	}
+	
 }
--- LanguageAnalysator/src/LanguageAnalysator.java
+++ LanguageAnalysator/src/LanguageAnalysator.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class LanguageAnalysator {
 
 	public static void main (String[]args){
-		ArrayList<String> words = FileInput.readFile("Deutch.txt");
+		ArrayList<String> words = FileInput.readFile("English.txt");
 		System.out.println(words);
 		System.out.println(words.get(1));
 		
@@ -11,9 +11,11 @@ public class LanguageAnalysator {
 //			System.out.println(str);;
 //		}
 		
-		Language german = new Language("German", words);
-		System.out.println(german.getContent());
-		
+		Language testLang = new Language("Test Lan", words);
+		//System.out.println(testLang.getContent());
+		//System.out.println(testLang.getString());
+		//System.out.println(testLang.getStringFirstChar());
+		testLang.getStringFirstChar();
 		
 	}
 	
